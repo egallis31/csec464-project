1bb74ddcad22f8456ec292422e45c894ee3ad895
Evan Gallis <evanrgallis@gmail.com>
2020-03-26 10:14:14 -0400

    test data type

--- server/utils/api_info.py
+++ server/utils/api_info.py
@@ -1,8 +1,7 @@
 # API INFO 
 routes = {
-    "routes": 
-        [
+    "routes": { 
             {"path": "/api/recv", "methods": "POST", "function": "recieve forensics data from collection source"}, 
             {"path": "/api/test-redis", "methods": "GET", "function": "Test Redis Connection"}
-        ]
+        }
     }
