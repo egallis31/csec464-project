2ec6ccdf01a0fdc6322b5207c907233e41a16838
Evan Gallis <evanrgallis@gmail.com>
2020-03-26 10:26:42 -0400

    test different data format

--- server/utils/api_info.py
+++ server/utils/api_info.py
@@ -6,3 +6,14 @@ routes = {
             {"path": "/api/test-redis", "methods": "GET", "function": "Test Redis Connection"}
         ]
     }
+
+test_routes = {
+    "/api/recv": {
+        "methods": "POST",
+        "function": "Recieve forensics data from collection source"
+    },
+    "/api/test-redis" : {
+        "methods": "GET",
+        "functions": "Test Redis connection"
+    }
+}
